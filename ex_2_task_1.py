99d1731932e799d830b2c2349a4d4766a16c6fca
Pinar <pinaradanir96@gmail.com>
2021-06-05 10:03:46 -0500

    works with my email address now!

--- ex_2_task_1.py
+++ ex_2_task_1.py
@@ -42,7 +42,9 @@ tech = email[1].split(".")
 hosting = tech[0]
 ending = tech[1]
 
-#print(username, hosting, ending)
+validending = ["com", "edu", "org", "gov"]
+
+print(username, hosting, ending)
 
 
 def is_valid_email_address(s):
@@ -55,12 +57,13 @@ def is_valid_email_address(s):
     if len(hosting) < 2 or len(hosting) > 8:
         print("Sorry, not a valid email hosting name.")
 
-    if ending != "com" or ending != "edu" or ending != "org" or ending != "gov":
+    if ending not in validending :
         print("Sorry, invalid ending.")
-        
+
     else:
         print("Valid email address! Thank you.")
 
+is_valid_email_address(s)
 
     # A must have between 3 and 16 alpha numeric chars (test: isalnum())
 # B must have between 2 and 8 alpha numeric chars (test: isalnum()) 
